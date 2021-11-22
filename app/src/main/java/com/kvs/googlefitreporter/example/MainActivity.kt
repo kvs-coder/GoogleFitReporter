caad7a41d3cf71c08f8b9ee83b769f886dab3ec1
kvs <victorkachalov@gmail.com>
2021-11-22 19:32:20 +0100

    testting gfit auth

--- app/src/main/java/com/kvs/googlefitreporter/example/MainActivity.kt
+++ app/src/main/java/com/kvs/googlefitreporter/example/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.kvs.googlefitreporter.example
 
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.kvs.googlefitreporter.GoogleFitReporter
+import com.kvs.googlefitreporter.GoogleFitReporter.Companion.GOOGLE_FIT_PERMISSIONS_REQUEST_CODE
 
 class MainActivity : AppCompatActivity() {
+    val reporter: GoogleFitReporter get() = GoogleFitReporter(this)
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        reporter.start()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (resultCode) {
+            Activity.RESULT_OK -> when (requestCode) {
+                GOOGLE_FIT_PERMISSIONS_REQUEST_CODE -> reporter.accessGoogleFit()
+                else -> {
+                    // Result wasn't from Google Fit
+                }
+            }
+            else -> {
+                // Permission not granted
+            }
+        }
     }
 }
