ed5b4ca71cbd97210627e03a1b282b5bde79308d
kvs <victorkachalov@gmail.com>
2021-11-26 21:47:09 +0100

    it is important to get strict date interval like from 23:59:59.999999 till actual time to get the historical value (for steps now) corresponding with GFit App

--- library/src/main/java/com/kvs/googlefitreporter/GoogleFitReader.kt
+++ library/src/main/java/com/kvs/googlefitreporter/GoogleFitReader.kt
@@ -6,10 +6,15 @@ import android.util.Log
 import androidx.annotation.RequiresApi
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.fitness.Fitness
+import com.google.android.gms.fitness.HistoryClient
+import com.google.android.gms.fitness.data.DataPoint
+import com.google.android.gms.fitness.data.DataSet
+import com.google.android.gms.fitness.data.DataSource
 import com.google.android.gms.fitness.data.DataType
 import com.google.android.gms.fitness.request.DataReadRequest
-import java.time.LocalDateTime
-import java.time.ZoneId
+import java.time.*
+import java.time.temporal.TemporalAccessor
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class GoogleFitReader(
@@ -19,7 +24,7 @@ class GoogleFitReader(
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun accessGoogleFit() {
-        val end = LocalDateTime.now()
+        val end = LocalDateTime.of(LocalDate.now(), LocalTime.MAX)
         val start = end.minusYears(1)
         val endSeconds = end.atZone(ZoneId.systemDefault()).toEpochSecond()
         val startSeconds = start.atZone(ZoneId.systemDefault()).toEpochSecond()
@@ -27,6 +32,7 @@ class GoogleFitReader(
         val readRequest = DataReadRequest.Builder()
             .aggregate(DataType.AGGREGATE_STEP_COUNT_DELTA)
             .setTimeRange(startSeconds, endSeconds, TimeUnit.SECONDS)
+            .enableServerQueries()
             .bucketByTime(1, TimeUnit.DAYS)
             .build()
         Fitness.getHistoryClient(activity, account)
@@ -34,8 +40,43 @@ class GoogleFitReader(
             .addOnSuccessListener { response ->
                 // Use response data here
                 Log.i("TAG", "OnSuccess()")
+                for (dataSet in response.buckets.flatMap { it.dataSets }) {
+                    dumpDataSet(dataSet)
+                }
             }
             .addOnFailureListener { e -> Log.d("TAG", "OnFailure()", e) }
+
+        Fitness.getHistoryClient(activity, account)
+            .readDailyTotal(DataType.AGGREGATE_STEP_COUNT_DELTA)
+            .addOnSuccessListener { dataSet ->
+                // Use response data here
+                Log.i("TAG", "OnSuccess()")
+                dumpDataSet(dataSet)
+            }
     }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun dumpDataSet(dataSet: DataSet) {
+        Log.i("TAG", "Data returned for Data type: ${dataSet.dataType.name}")
+        for (dp in dataSet.dataPoints) {
+            Log.i("TAG","Data point:")
+            Log.i("TAG","\tType: ${dp.dataType.name}")
+            Log.i("TAG","\tStart: ${dp.getStartTimeString()}")
+            Log.i("TAG","\tEnd: ${dp.getEndTimeString()}")
+            for (field in dp.dataType.fields) {
+                Log.i("TAG","\tField: ${field.name.toString()} Value: ${dp.getValue(field)}")
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun DataPoint.getStartTimeString() = Instant.ofEpochSecond(this.getStartTime(TimeUnit.SECONDS))
+        .atZone(ZoneId.systemDefault())
+        .toLocalDateTime().toString()
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun DataPoint.getEndTimeString() = Instant.ofEpochSecond(this.getEndTime(TimeUnit.SECONDS))
+        .atZone(ZoneId.systemDefault())
+        .toLocalDateTime().toString()
 
 }
---
+++ library/src/main/java/com/kvs/googlefitreporter/model/StepCount.kt
@@ -0,0 +1,5 @@
+package com.kvs.googlefitreporter.model
+
+class StepCount {
+
+}
